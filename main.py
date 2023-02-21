e1510200cc0b03e3e1a35327480873d98e4d3a13
John O'Brien <mesathus@bellsouth.net>
2023-02-21 17:49:46 -0500

    Database updates added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy
 import pandas
 import mysql.connector
 import itertools
+import csv
 import sqlite3
 
 
@@ -46,11 +47,40 @@ class ImdbCrawler(CrawlSpider):
 
 
 def updateDatabase():
-    x = 0
-
+    try:
+        cursor = con.cursor()
+        with open("excel files/all-weeks-countries.csv") as allCountries:
+            data = csv.DictReader(allCountries)
+            countriesInfo = [(i['country_name'], i['country_iso2'], i['week'], i['category'], i['weekly_rank'],
+                             i['show_title'], i['season_title'], i['cumulative_weeks_in_top_10']) for i in data]
+            cursor.executemany("INSERT INTO rankByCountry (country_name,country_iso2,week,category,"
+                               "weekly_rank,show_title,season_title,cumulative_weeks_in_top_10) VALUES "
+                               "(?,?,?,?,?,?,?,?);", countriesInfo)
+            allCountries.close()
+        with open("excel files/all-weeks-global.csv") as allGlobal:
+            data = csv.DictReader(allGlobal)
+            globalInfo = [(i['week'], i['category'], i['weekly_rank'], i['show_title'], i['season_title'],
+                           i['weekly_hours_viewed'], i['cumulative_weeks_in_top_10']) for i in data]
+            cursor.executemany("INSERT INTO rankByCountry (week,category,weekly_rank,show_title,season_title,"
+                               "weekly_hours_viewed,cumulative_weeks_in_top_10) VALUES "
+                               "(?,?,?,?,?,?,?);", globalInfo)
+            allGlobal.close()
+        with open("excel files/most-popular.csv") as popular:
+            data = csv.DictReader(popular)
+            popularInfo = [(i['category'], i['rank'], i['show_title'], i['season_title'],
+                            i['hours_viewed_first_28_days']) for i in data]
+            cursor.executemany("INSERT INTO rankByCountry (category,rank,show_title,season_title,"
+                               "hours_viewed_first_28_days) VALUES "
+                               "(?,?,?,?,?);", popularInfo)
+            popular.close()
     # TODO import the excel spreadsheet
     # TODO check most recent entry to see where to start in excel file / sort DB by date?
     # TODO crawl IMDB to get genre information
+    except sqlite3.DatabaseError as dbErr:
+        print(dbErr)
+    finally:
+        con.commit()
+        cursor.close()
 
 
 def genreCrawl(title):
@@ -59,18 +89,20 @@ def genreCrawl(title):
 
 
 def recommender():
-    x = 0
+    cursor = con.cursor()
     # TODO collect user input
     # TODO search DB for matching information and run regression
     # TODO make recommendations based on criteria (user specified?)
+    cursor.close()
 
 
 def initialize():
     try:
+        cursor = con.cursor()
         cursor.execute("SELECT * FROM rankByCountry")
     except sqlite3.DatabaseError as dbErr:
         print("Creating tables")
-        try:
+        try:  # TODO update script with extra fields fetched from spider and regenerate tables
             cursor.executescript("""
                        BEGIN;
                        CREATE TABLE rankByCountry(country_name,country_iso2,week,category,weekly_rank,show_title,"
@@ -80,20 +112,25 @@ def initialize():
                        COMMIT;""")
         except sqlite3.DatabaseError as dbErr2:
             print(dbErr2)
+    finally:
+        cursor.close()
 
 
 con = sqlite3.connect("netflix.db")
-cursor = con.cursor()
 
 
 def main():
     initialize()
+    cursor = con.cursor()
     results = cursor.execute("SELECT * FROM sqlite_master")
     # stub = open("excel files/all-weeks-countries.csv", "rt")
     stub2 = pandas.read_csv("excel files/all-weeks-countries.csv")
     # print(stub2)
     x = max(2, 4)
     print(x)
+    cursor.close()
+    if con:
+        con.close()
 
 
 main()
